8595c44d36128ea757a1a37cbe8bb9007d612ef8
oscarnava <oscarnava@gmail.com>
2019-11-28 18:48:11 -0600

    Set color and width by passing props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable class-methods-use-this */
 import React from 'react';
 
 export default class Button extends React.Component {
   render() {
+    const { props: { name, color = 'lightgray', wide = false } } = this;
+
     return (
-      <div className='button'>
-        {this.props.name}
+      <div className='button' style={{ backgroundColor: color, flexGrow: wide ? 1 : 0 }}>
+        {name}
       </div>
     );
   }
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -10,30 +10,30 @@ export default class ButtonPanel extends React.Component {
           <Button name='AC' />
           <Button name='+/-' />
           <Button name='%' />
-          <Button name='÷' />
+          <Button name='÷' color='orange' />
         </div>
         <div className='row'>
           <Button name='7' />
           <Button name='8' />
           <Button name='9' />
-          <Button name='x' />
+          <Button name='x' color='orange' />
         </div>
         <div className='row'>
           <Button name='4' />
           <Button name='5' />
           <Button name='6' />
-          <Button name='-' />
+          <Button name='-' color='orange' />
         </div>
         <div className='row'>
           <Button name='1' />
           <Button name='2' />
           <Button name='3' />
-          <Button name='+' />
+          <Button name='+' color='orange' />
         </div>
         <div className='row'>
-          <Button name='0' />
+          <Button name='0' wide={true}/>
           <Button name='.' />
-          <Button name='=' />
+          <Button name='=' color='orange' />
         </div>
       </div>
     );
